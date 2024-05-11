89bad5e8ad3c819241de05d882f819ad1b24955d
Alfen <alfenfajri@gmail.com>
2024-05-11 08:29:35 +0700

    merapihkan status

--- lib/pages/status_pesanan_page.dart
+++ lib/pages/status_pesanan_page.dart
@@ -51,108 +51,119 @@ class StatusPesananPage extends StatelessWidget {
 
                   // Menambahkan pengecekan untuk status "belum_bayar"
                   if (statusText == 'belum_bayar') {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Anda Belum Melakukan Pembayaran',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        SizedBox(height: 20),
-                        ElevatedButton(
-                          onPressed: () async {
-                            // Mengubah status menjadi "pesanan_diterima"
-                            await statusProvider.postPembayaran(
+                    return Center(
+                      // Menggunakan Center untuk menengahkan Column
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Anda Belum Melakukan Pembayaran',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: () async {
+                              // Mengubah status menjadi "pesanan_diterima"
+                              await statusProvider.postPembayaran(
                                 userId,
                                 Provider.of<AuthProvider>(context,
                                         listen: false)
-                                    .token);
-                          },
-                          child: Text('Lakukan Pembayaran'),
-                        ),
-                      ],
+                                    .token,
+                              );
+                            },
+                            child: Text('Lakukan Pembayaran'),
+                          ),
+                        ],
+                      ),
                     );
                   }
+
                   // Menambahkan pengecekan untuk status "sudah_bayar"
                   else if (statusText == 'sudah_bayar') {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Anda Sudah Bayar',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        SizedBox(height: 20),
-                        ElevatedButton(
-                          onPressed: () async {
-                            // Mengubah status menjadi "pesanan_diterima"
-                            await statusProvider.postPenjualTerima(
-                                userId,
-                                Provider.of<AuthProvider>(context,
-                                        listen: false)
-                                    .token);
-                          },
-                          child: Text('Pesanan Diterima Restoran'),
-                        ),
-                        SizedBox(height: 10),
-                        ElevatedButton(
-                          onPressed: () async {
-                            // Mengubah status menjadi "pesanan_ditolak"
-                            await statusProvider.postPenjualTolak(
-                                userId,
-                                Provider.of<AuthProvider>(context,
-                                        listen: false)
-                                    .token);
-                          },
-                          child: Text('Pesanan Ditolak Restoran'),
-                        ),
-                      ],
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Anda Sudah Bayar',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: () async {
+                              // Mengubah status menjadi "pesanan_diterima"
+                              await statusProvider.postPenjualTerima(
+                                  userId,
+                                  Provider.of<AuthProvider>(context,
+                                          listen: false)
+                                      .token);
+                            },
+                            child: Text('Pesanan Diterima Restoran'),
+                          ),
+                          SizedBox(height: 10),
+                          ElevatedButton(
+                            onPressed: () async {
+                              // Mengubah status menjadi "pesanan_ditolak"
+                              await statusProvider.postPenjualTolak(
+                                  userId,
+                                  Provider.of<AuthProvider>(context,
+                                          listen: false)
+                                      .token);
+                            },
+                            child: Text('Pesanan Ditolak Restoran'),
+                          ),
+                        ],
+                      ),
                     );
                   }
                   // Menambahkan pengecekan untuk status "pesanan_diterima" dan "pesanan_ditolak"
                   else if (statusText == 'pesanan_diterima') {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Anda Sudah Bayar dan Pesanan Diterima Restoran',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        SizedBox(height: 20),
-                        ElevatedButton(
-                          onPressed: () async {
-                            // Mengubah status menjadi "pesanan_diterima"
-                            await statusProvider.postDiantar(
-                                userId,
-                                Provider.of<AuthProvider>(context,
-                                        listen: false)
-                                    .token);
-                          },
-                          child: Text('Antar Makanan'),
-                        ),
-                      ],
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Anda Sudah Bayar dan Pesanan Diterima Restoran',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: () async {
+                              // Mengubah status menjadi "pesanan_diterima"
+                              await statusProvider.postDiantar(
+                                  userId,
+                                  Provider.of<AuthProvider>(context,
+                                          listen: false)
+                                      .token);
+                            },
+                            child: Text('Antar Makanan'),
+                          ),
+                        ],
+                      ),
                     );
                   } else if (statusText == 'pesanaan_diantar') {
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Pesanan Sedang Diantar Ke Lokasi Anda',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        SizedBox(height: 20),
-                        ElevatedButton(
-                          onPressed: () async {
-                            // Mengubah status menjadi "pesanan_diterima"
-                            await statusProvider.postDiterima(
-                                userId,
-                                Provider.of<AuthProvider>(context,
-                                        listen: false)
-                                    .token);
-                          },
-                          child: Text('Selesaikan Pesanan'),
-                        ),
-                      ],
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Pesanan Sedang Diantar Ke Lokasi Anda',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(height: 20),
+                          ElevatedButton(
+                            onPressed: () async {
+                              // Mengubah status menjadi "pesanan_diterima"
+                              await statusProvider.postDiterima(
+                                  userId,
+                                  Provider.of<AuthProvider>(context,
+                                          listen: false)
+                                      .token);
+                            },
+                            child: Text('Selesaikan Pesanan'),
+                          ),
+                        ],
+                      ),
                     );
                   } else if (statusText == 'pesanan_selesai') {
                     // Menambahkan penundaan 3 detik sebelum kembali ke halaman utama
@@ -163,20 +174,22 @@ class StatusPesananPage extends StatelessWidget {
                       );
                     });
 
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Pesanan Selesai Kembali Ke Halaman Awal untuk order',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        SizedBox(height: 20),
-                        Text(
-                          'Kembali ke Halaman Awal dalam 3 detik...',
-                          style: TextStyle(
-                              fontSize: 14, fontStyle: FontStyle.italic),
-                        ),
-                      ],
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Pesanan Selesai Kembali Ke Halaman Awal untuk order',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            'Kembali ke Halaman Awal dalam 3 detik...',
+                            style: TextStyle(
+                                fontSize: 14, fontStyle: FontStyle.italic),
+                          ),
+                        ],
+                      ),
                     );
                   } else if (statusText == 'pesanan_ditolak') {
                     // Menambahkan penundaan 3 detik sebelum kembali ke halaman utama
@@ -187,20 +200,22 @@ class StatusPesananPage extends StatelessWidget {
                       );
                     });
 
-                    return Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Pesanan Ditolak Restoran Kembali Ke Halaman Awal untuk order Uang Dikembalikan',
-                          style: TextStyle(fontSize: 16),
-                        ),
-                        SizedBox(height: 20),
-                        Text(
-                          'Kembali ke Halaman Awal dalam 3 detik...',
-                          style: TextStyle(
-                              fontSize: 14, fontStyle: FontStyle.italic),
-                        ),
-                      ],
+                    return Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Pesanan Ditolak Restoran Kembali Ke Halaman Awal untuk order Uang Dikembalikan',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            'Kembali ke Halaman Awal dalam 3 detik...',
+                            style: TextStyle(
+                                fontSize: 14, fontStyle: FontStyle.italic),
+                          ),
+                        ],
+                      ),
                     );
                   }
                   // Jika status bukan "belum_bayar", "sudah_bayar", "pesanan_diterima", atau "pesanan_ditolak"
